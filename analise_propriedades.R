2ab72ed18ba8d45202605f381a444241e27ed6c6
Edbbioeco <edsonbbiologia@gmail.com>
2025-12-21 15:26:52 -0300

    Primeiro commit

---
+++ analise_propriedades.R
@@ -0,0 +1,36 @@
+# Pacotes ----
+
+library(readxl)
+
+library(tidyverse)
+
+library(performance)
+
+# Dados ----
+
+## Importando ----
+
+dados <- readxl::read_xlsx("dados.xlsx")
+
+## Visualizando ----
+
+dados
+
+dados |> dplyr::glimpse()
+
+# Modelo do efeito do tipo de partida nas pedras de respiro ----
+
+## Criando o modelo ----
+
+lm_pedras <- lm(`Valores totais` ~ Tipo,
+                data = dados)
+
+## Pressupostos do modelo ----
+
+lm_pedras |> performance::check_model(check = c("qq",
+                                                "normality",
+                                                "homogeneity"))
+
+## Avalaindo o modelo ----
+
+lm_pedras |> summary()
