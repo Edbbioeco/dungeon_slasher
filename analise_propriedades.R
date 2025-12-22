9e68f9fa66b4c7a5169904c2c3fc5dc138952a4a
Edbbioeco <edsonbbiologia@gmail.com>
2025-12-22 09:04:56 -0300

    Atualização do script: 22.11.2025 09:04

--- analise_propriedades.R
+++ analise_propriedades.R
@@ -4,7 +4,11 @@ library(readxl)
 
 library(tidyverse)
 
+library(magrittr)
+
 library(performance)
+
+library(ggbeeswarm)
 
 library(ggview)
 
@@ -20,11 +24,16 @@ dados
 
 dados |> dplyr::glimpse()
 
+## Tratamdo ---
+
+dados %<>%
+  tidyr::drop_na()
+
 # Modelo do efeito do tipo de partida nas pedras de respiro ----
 
 ## Criando o modelo ----
 
-lm_pedras <- lm(`Valores totais` ~ Tipo,
+lm_pedras <- lm(`Valores Totais` ~ Tipo * `Quantidade de estagios`,
                 data = dados)
 
 ## Pressupostos do modelo ----
@@ -38,3 +47,15 @@ lm_pedras |> performance::check_model(check = c("qq",
 lm_pedras |> summary()
 
 ## Gráfiico ----
+
+dados |>
+  ggplot(aes(Tipo, `Valores Totais`, fill = `Quantidade de estagios`)) +
+  ggbeeswarm::geom_quasirandom(shape = 21, stroke = 1, size = 5) +
+  scale_fill_viridis_c(guide = guide_colourbar(title.hjust = 0.5,
+                                               barheight = 20)) +
+  ggview::canvas(height = 10, width = 12) +
+  theme_classic() +
+  theme(axis.text = element_text(color = "black", size = 17.5),
+        axis.title = element_text(color = "black", size = 17.5),
+        legend.text = element_text(color = "black", size = 17.5),
+        legend.title = element_text(color = "black", size = 17.5))
