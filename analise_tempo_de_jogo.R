2f52eec593a5d2d8a5868400fd4a2b8fdf44400a
Edbbioeco <edsonbbiologia@gmail.com>
2025-12-26 10:49:09 -0300

    Atualização do script: 26.12.2025 10:48
    Adicionei o gráfico da análise do efeito do tipo de estágio no tempo de jogo, assim como as estatísticas no gráfico.

--- analise_tempo_de_jogo.R
+++ analise_tempo_de_jogo.R
@@ -107,3 +107,72 @@ lm_tempo_tipo |>
   summary()
 
 ## Gráfico ----
+
+### Estatísticas das variáveis ----
+
+summary <- lm_tempo_tipo |>
+  summary()
+
+tabelaestatisticas <- summary$coefficients |>
+  as.data.frame() |>
+  tibble::rownames_to_column() |>
+  tibble::as_tibble() |>
+  dplyr::filter(!rowname |> stringr::str_detect("Intercept")) |>
+  dplyr::mutate(Tipo = 0.425,
+                Tempo = "1899-12-31 01:30:00" |> lubridate::as_datetime(),
+                `Quantidade de estagios` = NA,
+                estatistica = paste0(rowname,
+                                     ":<br>β1 ± SE = ",
+                                     Estimate |> round(2),
+                                     " ± ",
+                                     `Std. Error` |> round(2),
+                                     ",<br>t = ",
+                                     `t value` |> round(2),
+                                     ", p = ",
+                                     `Pr(>|t|)` |> round(2))) |>
+  dplyr::select(6:9)
+
+tabelaestatisticas
+
+### Estatística do modelo global ----
+
+f_global <- paste0("F<sub>",
+                   summary$fstatistic[[2]],
+                   ", ",
+                   summary$fstatistic[[3]],
+                   "</sub> = ",
+                   summary$fstatistic[[1]] |> round(2),
+                   ", p = 0.054, R² = ",
+                   summary$adj.r.squared |> round(2))
+
+f_global
+
+### Gráfico ----
+
+dados |>
+  ggplot(aes(Tipo, Tempo, fill = `Quantidade de estagios`)) +
+  ggbeeswarm::geom_quasirandom(shape = 21, stroke = 1, size = 5) +
+  ggtext::geom_richtext(data = tabelaestatisticas,
+                        aes(Tipo, Tempo, label = estatistica),
+                        fontface = "bold",
+                        fill = NA,
+                        size = 5,
+                        hjust = 0,
+                        label.color = NA) +
+  scale_fill_viridis_c(guide = guide_colourbar(title.position = "top",
+                                               title.hjust = 0.5,
+                                               barwidth = 25,
+                                               frame.colour = "black",
+                                               frame.linewidth = 1,
+                                               ticks.colour = "black",
+                                               ticks.linewidth = 1),
+                       breaks = seq(15, 24, 1)) +
+  labs(title = f_global) +
+  ggview::canvas(height = 10, width = 12) +
+  theme_classic() +
+  theme(axis.text = element_text(color = "black", size = 17.5),
+        axis.title = element_text(color = "black", size = 17.5),
+        legend.text = element_text(color = "black", size = 17.5),
+        legend.title = element_text(color = "black", size = 17.5),
+        legend.position = "bottom",
+        plot.title = ggtext::element_markdown(color = "black", size = 20))
